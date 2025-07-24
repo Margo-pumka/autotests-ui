34fb9fd2dc440ff4880ac28075658f58a89406c1
Rita <fedorova.margarita.a@gmail.com>
2025-07-25 00:16:05 +0300

    Практикуемся в интеграции покрытия в UI тесты. Практическое задание: Работа с инструментом ui-coverage-tool

--- elements/textarea.py
+++ elements/textarea.py
@@ -1,5 +1,6 @@
 import allure
 from playwright.sync_api import Locator, expect
+from ui_coverage_tool import ActionType
 
 from elements.base_element import BaseElement
 from logger import logger
@@ -13,6 +14,9 @@ class Textarea(BaseElement):
     def get_locator(self, nth: int = 0, **kwargs) -> Locator:
         return super().get_locator(nth, **kwargs).locator('textarea').first
 
+    def get_raw_locator(self, nth: int = 0, **kwargs) -> str:
+        return f'{super().get_raw_locator(**kwargs)}//textarea[{nth + 1}]'
+
     def fill(self, value: str, nth: int = 0, **kwargs):
         step = f'Fill {self.type_of} "{self.name}" to value "{value}"'
         with allure.step(step):
@@ -20,9 +24,13 @@ class Textarea(BaseElement):
             locator = self.get_locator(nth, **kwargs)
             locator.fill(value)
 
+        self.track_coverage(ActionType.FILL, nth, **kwargs)
+
     def check_have_value(self, value: str, nth: int = 0, **kwargs):
         step = f'Checking that {self.type_of} "{self.name}" has a value "{value}"'
         with allure.step(step):
             logger.info(step)
             locator = self.get_locator(nth, **kwargs)
             expect(locator).to_have_value(value)
+
+        self.track_coverage(ActionType.VALUE, nth, **kwargs)
